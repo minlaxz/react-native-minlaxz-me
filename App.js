1016994211ad13a2960d95d14e3be30647562833
minlaxz <minminlaxz@gmail.com>
2020-11-20 18:19:08 +0630

    Add webview
    webview with backbutton handler https://stackoverflow.com/a/61107584/10582082 .

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { WebView } from 'react-native-webview';
+import { StyleSheet, BackHandler } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  WEBVIEW_REF = React.createRef();
+  state = {
+    canGoBack: false,
+  };
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
+  }
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.handleBackButton);
+  }
+  handleBackButton = () => {
+    if (this.state.canGoBack) {
+      this.WEBVIEW_REF.current.goBack();
+      return true;
+    }
+  };
+  onNavigationStateChange = (navState) => {
+    this.setState({
+      canGoBack: navState.canGoBack,
+    });
+  };
+  render() {
+    return (
+      <>
+        <WebView
+          source={{ uri: 'https://minlaxz.me' }}
+          ref={this.WEBVIEW_REF}
+          onNavigationStateChange={this.onNavigationStateChange}
+          style={styles.container}
+        />
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f5f5',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop:10
   },
 });
